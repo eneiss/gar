c8db1e0727093e2340e2b5e2782e81b5e3e6c9c8
Emma Neiss <emmaneiss2@gmail.com>
2026-08-23 11:36:49 -0400

    Scaffold base archive and extract commands

---
+++ cmd/extract.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// extractCmd represents the extract command
+var extractCmd = &cobra.Command{
+	Use:   "extract",
+	Short: "Extract an archive",
+	Long: `Extract the target archive.
+For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("extract called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(extractCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// extractCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// extractCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2026 Emma Neiss
-
 */
 package cmd
 
@@ -10,18 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gar",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A basic Go implementation of the most useful GNU tar command features",
+	Long: `Gar implements archive creation and extraction, and optional gzip compression
+similaryly to GNU tar.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This application was built for learning purposes, not to be used anywhere serious!`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +42,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
