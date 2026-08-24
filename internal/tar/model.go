34f59ff7016742c4be40383592131c3de85f253f
Emma Neiss <emmaneiss2@gmail.com>
2026-08-24 00:20:04 -0400

    WIP build parsed POSIX header

--- internal/tar/model.go
+++ internal/tar/model.go
@@ -1,11 +1,35 @@
 package tar
 
+// Sources for specifications:
+// [1]: https://ftp.gnu.org/old-gnu/Manuals/tar-1.12/html_node/tar_123.html
+// [2]: https://mort.coffee/home/tar/
+
 import (
 	"fmt"
 	"github.com/eneiss/gar/internal/utils"
+	"time"
 )
 
+type typeFlag byte
+
+const (
+	RegType   typeFlag = '0' // regular file
+	ARegType  typeFlag = 0x0 // backward-compatible variant for regular file, NUL byte
+	LinkType  typeFlag = '1' // link (any type)
+	SymType   typeFlag = '2' // symlink
+	CharType  typeFlag = '3' // character special
+	BlockType typeFlag = '4' // block special
+	DirType   typeFlag = '5' // directory
+	FifoType  typeFlag = '6' // FIFO special
+	ContType  typeFlag = '7' // reserved
+)
+
+// TODO: parse into usable data structures (especially for integer values?)
 type PosixHeader struct {
+	// The name, linkname, magic, uname, and gname are null-terminated character
+	// strings. All other fileds are zero-filled octal numbers in ASCII. Each
+	// numeric field of width w contains w minus 2 digits, a space, and a null,
+	// except size, and mtime, which do not contain the trailing null. [1]
 	Name     [100]byte
 	Mode     [8]byte
 	Uid      [8]byte
@@ -15,7 +39,8 @@ type PosixHeader struct {
 	Chksum   [8]byte
 	Typeflag byte
 	Linkname [100]byte
-	// NOTE: the following are replaced by padding [255]byte
+	// NOTE: the following are replaced by Padding [255]byte (source: [2])
+	// also makes it simpler
 	// magic    [6]byte
 	// version  [2]byte
 	// uname    [32]byte
@@ -26,11 +51,25 @@ type PosixHeader struct {
 	Padding [255]byte
 }
 
-func BuildHeader(raw_input []byte) (*PosixHeader, error) {
+type ParsedPosixHeader struct {
+	Name     string
+	Mode     [7]byte // TBD, maybe octal string is fine
+	Uid      int
+	Gid      int
+	Size     int64 // NOTE: 8^12 = 2^36 > size of int (32 bits)
+	Mtime    int64
+	Chksum   int
+	Typeflag typeFlag
+	Linkname string
+	Padding  [255]byte // TBD
+}
+
+func BuildRawHeader(raw_input []byte) (*PosixHeader, error) {
 	if len(raw_input) < 255 {
 		return nil, fmt.Errorf("cannot build tar header from raw input: input too short (expected at least 255 bytes, got %d)", len(raw_input))
 	}
 
+	// Source: [1]
 	header := PosixHeader{
 		Name:     [100]byte(raw_input[0:100]),
 		Mode:     [8]byte(raw_input[100:108]),
@@ -47,11 +86,95 @@ func BuildHeader(raw_input []byte) (*PosixHeader, error) {
 	return &header, nil
 }
 
+func (r PosixHeader) ParseRawHeader() *ParsedPosixHeader {
+	res := ParsedPosixHeader{
+		Name:     string(r.Name[:99]),
+		Mode:     [7]byte(r.Mode[:7]),
+		Typeflag: typeFlag(r.Typeflag),
+		Linkname: string(r.Linkname[:99]),
+		Padding:  r.Padding,
+	}
+	// Processed fields (octal base conversion)
+	// Uid
+	uid, err := utils.Base8ToBase10(r.Uid[:7])
+	if err != nil {
+		return nil
+	}
+	res.Uid = int(uid)
+	// Gid
+	gid, err := utils.Base8ToBase10(r.Gid[:7])
+	if err != nil {
+		return nil
+	}
+	res.Gid = int(gid)
+	// Size
+	size, err := utils.Base8ToBase10(r.Size[:11])
+	if err != nil {
+		return nil
+	}
+	res.Size = int64(size)
+	// Mtime
+	mtime, err := utils.Base8ToBase10(r.Mtime[:11])
+	if err != nil {
+		return nil
+	}
+	res.Mtime = int64(mtime)
+	// Chksum
+	chksum, err := utils.Base8ToBase10(r.Chksum[:6])
+	if err != nil {
+		return nil
+	}
+	res.Chksum = int(chksum)
+
+	return &res
+}
+
 // Prints the content of the POSIX header h in a human-friendly way
 func (h PosixHeader) Print() {
 	fmt.Printf("File name: %s\n", h.Name)
 	fmt.Printf("Mode: %s\n", h.Mode[0:8])
-	fmt.Printf("Uid: %d\n", utils.Base8ToBase10(h.Uid[:7]))
-	fmt.Printf("Gid: %d\n", utils.Base8ToBase10(h.Gid[:7]))
-	fmt.Printf("Size: %d bytes\n", utils.Base8ToBase10(h.Size[:11]))
+
+	base10Uid, err := utils.Base8ToBase10(h.Uid[:7])
+	if err != nil {
+		fmt.Printf("Error while displaying header uid: %v", err)
+		return
+	}
+	fmt.Printf("Uid: %d (ASCII: %s)\n", base10Uid, h.Uid)
+
+	base10Gid, err := utils.Base8ToBase10(h.Gid[:7])
+	if err != nil {
+		fmt.Printf("Error while displaying header gid: %v", err)
+		return
+	}
+	fmt.Printf("Gid: %d (ASCII: %s)\n", base10Gid, h.Gid)
+
+	base10Size, err := utils.Base8ToBase10(h.Size[:11])
+	if err != nil {
+		fmt.Printf("Error while displaying header size: %v", err)
+		return
+	}
+	fmt.Printf("Size: %d (ASCII: %s)\n", base10Size, h.Size)
+
+	base10Mtime, err := utils.Base8ToBase10(h.Mtime[:11])
+	if err != nil {
+		fmt.Printf("Error while displaying header mtime: %v", err)
+		return
+	}
+	fmt.Printf("Modification time: %s (ASCII: %s)\n", time.Unix(int64(base10Mtime), 0), h.Mtime)
+
+	base10Checksum, err := utils.Base8ToBase10(h.Chksum[:6]) // last 2 bytes are NUL and SP (space)
+	if err != nil {
+		fmt.Printf("Error while displaying header chksum: %v", err)
+		return
+	}
+	fmt.Printf("Checksum (base 10 result): %d (ASCII: %s)\n", base10Checksum, h.Chksum)
+
+	fmt.Printf("File type: %c\n", h.Typeflag)
 }
+
+// TODO: checksum of header to check for data corruption
+// The chksum field is the ASCII representation of the octal value of the
+// simple sum of all bytes in the header block. Each 8-bit byte in the header
+// is added to an unsigned integer, initialized to zero, the precision of which
+// shall be no less than seventeen bits. When calculating the checksum, the
+// chksum field is treated as if it were all blanks. [1]
--- internal/utils/conversions.go
+++ internal/utils/conversions.go
@@ -1,25 +1,30 @@
 package utils
 
-// import "math"
-// nah we're doing bit shifts instead
+import (
+	"fmt"
+	// "math"
+	// nah we're doing bit shifts instead
+)
 
 // Takes an array of bytes representing a base 8 number, and returns
 // the base 10 equivalent as an integer.
-// Does NOT check for overflow with bit shifts!!
-func Base8ToBase10(bytes []byte) int {
-	res := 0
+// Does NOT check for overflow with bit shifts, uses uint64 to guard against
+// most cases instead.
+func Base8ToBase10(bytes []byte) (uint64, error) {
+	var res uint64 = 0
 
 	length := len(bytes)
 
 	for i, b := range bytes {
+
+		if b < '0' || b > '7' {
+			return 0, fmt.Errorf("invalid base 8 character: %c", b)
+		}
 		// bit shift shenanigans to multiply by powers of 8:
 		// multiplying by 8 means shifting bits to the left 3 times, without overflow considerations
 		// and substracting 0x30 to go from ASCII code to raw integer value
-		res += int(b-byte(0x30)) << (3 * (length - i - 1))
-
-		// easier to understand, worse efficiency
-		// res += (int(b) - 0x30) * int(math.Pow(float64(8), float64(length-i-1)))
+		res += uint64(b-'0') << (3 * (length - i - 1))
 	}
 
-	return res
+	return res, nil
 }
